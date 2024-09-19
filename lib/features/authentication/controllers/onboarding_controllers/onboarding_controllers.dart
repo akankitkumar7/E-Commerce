cac3e634d8fbbea898d614724f99b2e3a5082359
akankitkumar7 <ak4752970@gmai.com>
2024-09-19 21:41:08 +0530

    "second commit"

--- lib/features/authentication/controllers/onboarding_controllers/onboarding_controllers.dart
+++ lib/features/authentication/controllers/onboarding_controllers/onboarding_controllers.dart
@@ -27,6 +27,7 @@ class OnBoardingController extends GetxController{
 
   void nextPage(){
      if(currentPageIndex.value == 2){
+       /// when you click on skip it "isFirstTime become false and next time you will not see any onboarding screens "
        final storage = GetStorage();
        storage.write("isFirstTime", false);
        Get.offAll(const LoginScreen());
